9df12ac285d7b45297173d7488a0e8f7fb6337fd
Kishor Balgi <75678927+KishorBalgi@users.noreply.github.com>
2022-06-24 21:07:02 +0530

    Added map functionalities (#3)
    Added some map functionalities like custom marker creates and user geo location reader

--- static/scripts/map.js
+++ static/scripts/map.js
@@ -1,4 +1,4 @@
-let curLatLang = [12.985409023466968, 77.58148936513626];
+let curLatLang = [12.933969688632496, 77.61193685079267];
 
 var map = L.map("map").setView(curLatLang, 13);
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -14,6 +14,55 @@ var LeafIcon = L.Icon.extend({
     popupAnchor: [0, -32],
   },
 });
+
+// Get user geolocation:
+function getCurrLoc() {
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(function (pos) {
+      console.log(pos.coords.latitude, pos.coords.longitude);
+      curLatLang = [pos.coords.latitude, pos.coords.longitude];
+      // loadMap(curLatLang);
+    });
+  } else {
+    alert("Browser doesnot support geolocation");
+  }
+}
+function currentLocMarker(curLoc) {
+  // var marker = L.marker(curLoc, {
+  //   icon: new LeafIcon({
+  //     iconUrl: `${window.location.origin}/static/icons/map/my-location.png`,
+  //   }),
+  // }).addTo(map);
+  // marker.bindPopup("I am here");
+  L.circle(curLoc, { radius: 100 }).addTo(map);
+  map.panTo(new L.LatLng(...curLoc));
+}
+
+function fitMarkers(markers) {
+  var group = new L.featureGroup(markers);
+  map.fitBounds(group.getBounds());
+}
+
+function addMarker(latLng, icon) {
+  var marker = L.marker(latLng, {
+    icon: new LeafIcon({
+      iconUrl: `${window.location.origin}/static/icons/map/${icon}.png`,
+    }),
+  }).addTo(map);
+}
+
+function addMarkers(data, icon) {
+  markers = [];
+  data.forEach((e) => {
+    m = L.marker(e, {
+      icon: new LeafIcon({
+        iconUrl: `${window.location.origin}/static/icons/map/${icon}.png`,
+      }),
+    }).addTo(map);
+    markers.push(m);
+  });
+  fitMarkers(markers);
+}
 
 // Data:
 const food = [
@@ -39,59 +88,37 @@ const bus = [
   [12.949191766273346, 77.57360388117644],
   [12.949324781511196, 77.57379811157111],
 ];
+addMarkers(food, "restaurant");
+addMarkers(bus, "bus");
+addMarkers(shops, "shop");
 
-map.panTo(new L.LatLng(12.947962836536151, 77.57231830099437));
-var marker = L.marker([12.947962836536151, 77.57231830099437], {
-  icon: new LeafIcon({
-    iconUrl: `${window.location.origin}/static/icons/map/my-location.png`,
-  }),
-}).addTo(map);
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.");
-L.circle([12.947962836536151, 77.57231830099437], { radius: 100 }).addTo(map);
+// map.panTo(new L.LatLng(12.947962836536151, 77.57231830099437));
+// var marker = L.marker([12.947962836536151, 77.57231830099437], {
+//   icon: new LeafIcon({
+//     iconUrl: `${window.location.origin}/static/icons/map/my-location.png`,
+//   }),
+// }).addTo(map);
+// marker.bindPopup("<b>Hello world!</b><br>I am a popup.");
+// L.circle([12.947962836536151, 77.57231830099437], { radius: 100 }).addTo(map);
 
-// Create markers:
-food.forEach((e) => {
-  L.marker(e, {
-    icon: new LeafIcon({
-      iconUrl: `${window.location.origin}/static/icons/map/restaurant.png`,
-    }),
-  }).addTo(map);
-});
+// const routing = L.Routing.control({
+//   waypoints: [
+//     L.latLng(15.823532842591865, 74.5033861005741),
+//     L.latLng(15.819472137774307, 74.50183027558413),
+//     L.latLng(15.818778585138219, 74.50519060954872),
+//   ],
+//   // ,show:false
+// })
+//   .on("routesfound", function (e) {
+//     // showDirections(e);
+//   })
+//   .addTo(map);
 
-shops.forEach((e) => {
-  L.marker(e, {
-    icon: new LeafIcon({
-      iconUrl: `${window.location.origin}/static/icons/map/shop.png`,
-    }),
-  }).addTo(map);
-});
-
-bus.forEach((e) => {
-  L.marker(e, {
-    icon: new LeafIcon({
-      iconUrl: `${window.location.origin}/static/icons/map/bus.png`,
-    }),
-  }).addTo(map);
-});
-
-const routing = L.Routing.control({
-  waypoints: [
-    L.latLng(15.823532842591865, 74.5033861005741),
-    L.latLng(15.819472137774307, 74.50183027558413),
-    L.latLng(15.818778585138219, 74.50519060954872),
-  ],
-  // ,show:false
-})
-  .on("routesfound", function (e) {
-    // showDirections(e);
-  })
-  .addTo(map);
-
-const mapDir = document.getElementById("pills-directions");
-var routingControlContainer = routing.getContainer();
-var controlContainerParent = routingControlContainer.parentNode;
-controlContainerParent.removeChild(routingControlContainer);
-mapDir.appendChild(routingControlContainer.childNodes[0]);
+// const mapDir = document.getElementById("pills-directions");
+// var routingControlContainer = routing.getContainer();
+// var controlContainerParent = routingControlContainer.parentNode;
+// controlContainerParent.removeChild(routingControlContainer);
+// mapDir.appendChild(routingControlContainer.childNodes[0]);
 
 // Functions:
 function showDirections(e) {
@@ -102,34 +129,52 @@ function showDirections(e) {
   });
 }
 
-// Get user geolocation:
-// if ("geolocation" in navigator) {
-//   navigator.geolocation.getCurrentPosition(function (pos) {
-//     console.log(pos.coords.latitude, pos.coords.longitude);
-//     curLatLang = [pos.coords.latitude, pos.coords.longitude];
-//     loadMap(curLatLang);
-//   });
-// } else {
-//   alert("Browser doesnot support geolocation");
-// }
+function createWaypoints(latLngArr) {
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(function (pos) {
+      curLatLang = [pos.coords.latitude, pos.coords.longitude];
+      latLngArr = [curLatLang, ...latLngArr];
+      addMarkers(latLngArr, "marker");
+      latLngArr = latLngArr.map((l) => L.latLng(...l));
+      const routing = L.Routing.control({
+        waypoints: latLngArr,
+        createMarker: function () {
+          return null;
+        },
+      }).addTo(map);
+      currentLocMarker(curLatLang);
+      const mapDir = document.getElementById("pills-directions");
+      var routingControlContainer = routing.getContainer();
+      var controlContainerParent = routingControlContainer.parentNode;
+      controlContainerParent.removeChild(routingControlContainer);
+      mapDir.appendChild(routingControlContainer.childNodes[0]);
+    });
+  } else {
+    alert("Browser doesnot support geolocation");
+  }
+}
 
-// Load User Location:
-// function loadMap() {
-//   map.panTo(new L.LatLng(...curLatLang));
-//   var marker = L.marker(curLatLang, {
-//     icon: new LeafIcon({
-//       iconUrl: `${window.location.origin}/static/icons/my-location.png`,
-//     }),
-//   }).addTo(map);
-//   marker.bindPopup("<b>Hello world!</b><br>I am a popup.");
-//   L.circle(curLatLang, { radius: 200 }).addTo(map);
+// MAP API:
+function getTour(id) {
+  const url = `${window.location.origin}/api/v1/tour/${+id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      data = JSON.parse(data);
+      return data;
+    })
+    .catch((err) => console.log(err));
+}
 
-//   L.Routing.control({
-//     waypoints: [
-//       L.latLng(pos.coords.latitude, pos.coords.longitude),
-//       L.latLng(15.823532842591865, 74.5033861005741),
-//       L.latLng(15.819472137774307, 74.50183027558413),
-//       L.latLng(15.818778585138219, 74.50519060954872),
-//     ],
-//   }).addTo(map);
-// }
+function getDummyLatLng() {
+  const url = `${window.location.origin}/api/v1/dummy`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      data = JSON.parse(data);
+      console.log(data[0].fields);
+      return data;
+    })
+    .catch((err) => console.log(err));
+}
+getDummyLatLng();
