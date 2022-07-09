94a56c432793878c8015dea3f5c03eb4d14235ee
Kishor Balgi <kishorbalgi@gmail.com>
2022-07-09 22:55:20 +0530

    Resolved Conflicts

--- base/admin.py
+++ base/admin.py
@@ -1,28 +1,12 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from .models import Tour,Restaurant,TourReviews,Hotel,RepairShop,DummyLatLng,RegisteredBusiness
-=======
-<<<<<<< HEAD
-from .models import Tour,Restaurants,Reviews,Hotel,DummyLatLng,Users
-=======
-from .models import Tour,Restaurant,Reviews,Hotel,RepairShop,DummyLatLng,RegisteredBusiness,Users
->>>>>>> main
->>>>>>> 435bd44723f39b4550e9798d399fcb234d2ea96a
+from .models import Tour,Restaurant,Reviews,Hotel,RepairShop,DummyLatLng,RegisteredBusiness,Users,TourReviews
+
 # Register your models here.
 admin.site.register(Tour)
 admin.site.register(Restaurant)
 admin.site.register(TourReviews)
 admin.site.register(Hotel)
-<<<<<<< HEAD
-admin.site.register(DummyLatLng)
-admin.site.register(Users)
-=======
 admin.site.register(Users)
 admin.site.register(RepairShop)
 admin.site.register(RegisteredBusiness)
-<<<<<<< HEAD
 admin.site.register(DummyLatLng)
-=======
-admin.site.register(DummyLatLng)
->>>>>>> main
->>>>>>> 435bd44723f39b4550e9798d399fcb234d2ea96a
