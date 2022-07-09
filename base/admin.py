7590afdba93e1d70b1cc448ef097b55253632d56
Kishor Balgi <kishorbalgi@gmail.com>
2022-07-09 15:10:23 +0530

    Revert "Resolved Conflicts"
    This reverts commit 59dd209cba02f63ae5cff07d50875635220797e1.

--- base/admin.py
+++ base/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Tour,Restaurants,Reviews,Hotel,DummyLatLng
+from .models import Tour,Restaurants,Reviews,Hotel,DummyLatLng,Users
 # Register your models here.
 admin.site.register(Tour)
 admin.site.register(Restaurants)
 admin.site.register(Reviews)
 admin.site.register(Hotel)
 admin.site.register(DummyLatLng)
+admin.site.register(Users)
