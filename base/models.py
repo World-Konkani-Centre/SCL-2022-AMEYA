2628a63db2344aeeecded7bc59873abc34af15ff
AshwinShanbhag <ashshanbhag9945@gmail.com>
2022-07-12 21:14:54 +0530

    updated userprofile css

--- base/models.py
+++ base/models.py
@@ -59,7 +59,7 @@ class Profile(models.Model):
     def save(self, *args, **kwargs):
         super(Profile, self).save(*args, **kwargs)
         
-        img= Image.open(self.image.path)
+        img= Image.open(self.image.path)    
 
         if img.height > 300 or img.width > 300:
             output_size = (300,300)
