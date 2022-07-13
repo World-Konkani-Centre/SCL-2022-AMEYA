d361b9c6b6cea89abd1bb2bd4fd0991042cc26d4
AshwinShanbhag <ashshanbhag9945@gmail.com>
2022-07-13 21:05:08 +0530

    update userprofile backend

--- base/forms.py
+++ base/forms.py
@@ -5,10 +5,10 @@ from .models import Profile
 
 
 class UserUpdateForm(forms.ModelForm):
-    email =forms.EmailField(max_length=30, widget=forms.EmailInput(attrs={ 'class' : 'form-control'}))
-    username =forms.CharField(widget=forms.TextInput(attrs={ 'class' : 'form-control'}))
-    first_name =forms.CharField(widget=forms.TextInput(attrs={ 'class' : 'form-control'}))
-    last_name =forms.CharField(widget=forms.TextInput(attrs={ 'class' : 'form-control'}))
+    email =forms.EmailField(max_length=30, widget=forms.EmailInput(attrs={ 'class' : 'form-control', 'placeholder':'Email'})) 
+    username =forms.CharField(widget=forms.TextInput(attrs={ 'class' : 'form-control', 'placeholder':'Username'}))
+    first_name =forms.CharField(widget=forms.TextInput(attrs={ 'class' : 'form-control', 'placeholder':'First Name'}))
+    last_name =forms.CharField(widget=forms.TextInput(attrs={ 'class' : 'form-control', 'placeholder':'Last Name'}))
     
     class Meta:
         model = User
@@ -19,9 +19,9 @@ class UserUpdateForm(forms.ModelForm):
 
 
 class ProfileUpdateForm(forms.ModelForm):
-    phone =forms.CharField(max_length=10, widget=forms.TextInput(attrs={ 'class' : 'form-control'}))
-    DOB =forms.DateField(widget=forms.DateInput(attrs={ 'class' : 'form-control'}))
-    gender =forms.Select(attrs={ 'class' : 'form-control'})
+    phone =forms.CharField(max_length=10, min_length=10, widget=forms.TextInput(attrs={ 'class' : 'form-control','placeholder':'phone number','type':'number'}))
+    DOB =forms.DateField(widget=forms.DateInput(attrs={ 'class' : 'form-control','placeholder':'DD-MM-YYYY','type':'date','autoclose': True}))
+    gender =forms.Select(attrs={'class': 'form-control'})
     role =forms.Select(attrs={ 'class' : 'form-control'})   
 
     class Meta:
--- base/models.py
+++ base/models.py
@@ -47,10 +47,10 @@ class Profile(models.Model):
     id=models.BigAutoField(primary_key=True)
     role_choices=[('1','User'),('2','Business')]
     gender_choices=[('1','Male'),('2','Female'),('3','Dont want to specify')]
-    gender=models.CharField(blank=True, max_length=1,choices=gender_choices,default='1')
+    gender=models.CharField(max_length=1,choices=gender_choices,default='1')
     phone=models.CharField(blank=True, max_length=10,default='')
-    DOB=models.DateField(default='2001-01-01')
-    role=models.CharField(blank=True, max_length=1,choices=role_choices,default='1')
+    DOB=models.DateField(null=True)
+    role=models.CharField(max_length=1,choices=role_choices,default='1')
     image=models.ImageField(upload_to='profile_pics', height_field=None, width_field=None, max_length=100,default='default.jpg')
 
     def __str__(self):
