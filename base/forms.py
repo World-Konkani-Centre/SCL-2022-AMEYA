589acc52dc9317d000ad68fed48bba20e6455623
AshwinShanbhag <ashshanbhag9945@gmail.com>
2022-07-15 22:48:04 +0530

    Added css to userprofile

--- base/forms.py
+++ base/forms.py
@@ -5,24 +5,25 @@ from .models import Profile
 
 
 class UserUpdateForm(forms.ModelForm):
-    email =forms.EmailField(max_length=30, widget=forms.EmailInput(attrs={ 'class' : 'form-control', 'placeholder':'Email','readonly':True})) 
-    username =forms.CharField(widget=forms.TextInput(attrs={ 'class' : 'form-control', 'placeholder':'Username'}))
-    first_name =forms.CharField(widget=forms.TextInput(attrs={ 'class' : 'form-control', 'placeholder':'First Name'}))
-    last_name =forms.CharField(widget=forms.TextInput(attrs={ 'class' : 'form-control', 'placeholder':'Last Name'}))
+    email =forms.EmailField(max_length=30, widget=forms.EmailInput(attrs={ 'class' : 'form-control my-2', 'placeholder':'Email','readonly':True})) 
+    username =forms.CharField(widget=forms.TextInput(attrs={ 'class' : 'form-control my-2', 'placeholder':'Username'}))
+    first_name =forms.CharField(widget=forms.TextInput(attrs={ 'class' : 'form-control my-2', 'placeholder':'First Name'}))
+    last_name =forms.CharField(widget=forms.TextInput(attrs={ 'class' : 'form-control my-2', 'placeholder':'Last Name'}))
     
     class Meta:
         model = User
         fields = ['username','email','first_name','last_name'] 
     
 
-
 class ProfileUpdateForm(forms.ModelForm):
-    phone =forms.CharField(max_length=10, min_length=10, widget=forms.TextInput(attrs={ 'class' : 'form-control','placeholder':'phone number','type':'number'}))
-    DOB =forms.DateField(widget=forms.DateInput(attrs={ 'class' : 'form-control','placeholder':'DD-MM-YYYY','type':'date','autoclose': True}))
-    gender =forms.Select(attrs={'class': 'form-control'})
-    role =forms.Select(attrs={ 'class' : 'form-control'})   
-
+    phone =forms.CharField(max_length=10, min_length=10, widget=forms.TextInput(attrs={ 'class' : 'form-control my-2','placeholder':'phone number','type':'number'}))
+    DOB =forms.DateField(widget=forms.DateInput(attrs={ 'class' : 'form-control my-2','placeholder':'DD-MM-YYYY','type':'date','autoclose': True}))
+    gender_choices=[('1','Male'),('2','Female'),('3','Dont want to specify')]
+    role_choices=[('1','User'),('2','Business')]
+    gender =forms.ChoiceField(choices=gender_choices , widget=forms.Select(attrs={'class': 'form-control my-2'}))
+    #role =forms.ChoiceField(choices=role_choices , widget=forms.Select(attrs={'class': 'form-control my-1'}))
+    
     class Meta:
         model = Profile
-        fields = ['phone','DOB','gender','role','image']
+        fields = ['phone','DOB','gender','image']
 
