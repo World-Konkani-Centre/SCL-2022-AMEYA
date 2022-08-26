4d2ca1adb0d5b19e2b7239bbd3d1531d6ac82cba
Kishor Balgi <75678927+KishorBalgi@users.noreply.github.com>
2022-08-26 23:07:47 +0530

    Readme (#90)
    * Minor Bug fix
    
    * Updated readme
    
    * Updated Readme

--- YatraMitra/settings.py
+++ YatraMitra/settings.py
@@ -139,7 +139,7 @@ MEDIA_URL = '/media/'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER= 'yatramitra.app@gmail.com'
-EMAIL_HOST_PASSWORD = 'dxigbxwyzpldoefy'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 PASSWORD_RESET_TIMEOUT = 900 #  in seconds # 15 min 
 
